817d607619536d59e84cb4533a5aa2c17c00af18
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2025-09-10 13:15:14 +0200

    feat: add get_users endpoint to V1UserController
    Signed-off-by: Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>

--- src/Manager/User/Infrastructure/Controller/V1/V1UserController.php
+++ src/Manager/User/Infrastructure/Controller/V1/V1UserController.php
@@ -12,5 +12,12 @@ use Symfony\Component\Routing\Attribute\Route;
 
 #[Route( '/v1/users' )]
 final class V1UserController extends AbstractController {
+    #[Route( '/', name: 'get_users', methods: [ 'GET' ] )]
+    public function get_users(): JsonResponse {
+
+        return new JsonResponse( [
+            'message' => 'List all users',
+        ] );
+    }
 
 }
