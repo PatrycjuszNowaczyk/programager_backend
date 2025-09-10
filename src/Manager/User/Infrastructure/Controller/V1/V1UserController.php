8a69ee1198b4a11ab2074797dadaf74eb80468a1
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2025-09-10 12:53:14 +0200

    feat: add V1UserController with base routing configuration
    Signed-off-by: Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>

---
+++ src/Manager/User/Infrastructure/Controller/V1/V1UserController.php
@@ -0,0 +1,13 @@
+<?php
+
+declare( strict_types = 1 );
+
+namespace App\Manager\User\Infrastructure\Controller\V1;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Attribute\Route;
+
+#[Route( '/v1/users' )]
+final class V1UserController extends AbstractController {
+
+}
