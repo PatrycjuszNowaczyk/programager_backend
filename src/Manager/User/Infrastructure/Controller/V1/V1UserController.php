b8d0eeb6b66d3af4431b29859c7d24f968f4ddaa
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2025-09-10 12:57:38 +0200

    feat: include JsonResponse, Request, and UserDto in V1UserController
    Signed-off-by: Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>

--- src/Manager/User/Infrastructure/Controller/V1/V1UserController.php
+++ src/Manager/User/Infrastructure/Controller/V1/V1UserController.php
@@ -4,7 +4,10 @@ declare( strict_types = 1 );
 
 namespace App\Manager\User\Infrastructure\Controller\V1;
 
+use App\Manager\User\Infrastructure\Dto\UserDto;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Attribute\Route;
 
 #[Route( '/v1/users' )]
