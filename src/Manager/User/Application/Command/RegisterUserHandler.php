494692e62bb0f479cb3fc9bbb66cdf24fafcf042
Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>
2025-09-15 13:56:33 +0200

    feat: implement RegisterUserHandler boilerplate to process user registration commands
    Signed-off-by: Patrycjusz Nowaczyk <patrycjusz.nowaczyk@gmail.com>

---
+++ src/Manager/User/Application/Command/RegisterUserHandler.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Manager\User\Application\Command;
+
+use App\Manager\User\Domain\Enums\Role;
+use App\Manager\User\Domain\User;
+use App\Manager\User\Domain\ValueObject\VOEmail;
+use App\Manager\User\Domain\ValueObject\VOPasswordHash;
+use App\Manager\User\Domain\ValueObject\VOUserId;
+use DateTimeImmutable;
+use Symfony\Component\Messenger\Attribute\AsMessageHandler;
+
+#[AsMessageHandler( bus: 'command.bus' )]
+final class RegisterUserHandler {
+
+    public function __construct() {
+    }
+
+    public function __invoke( RegisterUserCommand $command ): string {
+        $user = User::register(
+            new VOUserId( uniqid() ),
+            new VOEmail( $command->email ),
+            new VOPasswordHash( $command->password ),
+            Role::USER,
+            new DateTimeImmutable(),
+        );
+
+        return "User registered with email: {$user->getEmail()} and password: {$user->getPasswordHash()}";
+    }
+}
